aef09885f8ae3e041cbe8c4da9ae6ffdc7d05a10
Ashen Cone <ashencone@gmail.com>
2022-05-22 11:40:30 +0530

    Init(zsh): zsh configuration

---
+++ zsh/zshrc.zsh
@@ -0,0 +1,31 @@
+# Zinit installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+# Load zinit
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+
+# Load important zinit annexes
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+# Core plugins
+zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+
+# Starship prompt
+eval "$(starship init zsh)"
+
+# Command alias
+alias ls=exa
+
+# Enable completions
+autoload -U compinit && compinit
